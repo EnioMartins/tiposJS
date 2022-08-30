da7f323e275eed4488bc1bb11530df741b25a737
EnioMartins <eniomartins7985@gmail.com>
2022-08-30 14:15:33 -0300

    Primeira versao tipos

---
+++ Tipos/type-number.js
@@ -0,0 +1,13 @@
+const numero1 = 1;
+const numero2 = 2;
+
+const resultado = numero1 + numero2;
+
+ console.log(resultado);
+
+ const numeroFlutuante = 2.1;
+ const operacao = resultado / numeroFlutuante;
+
+ 
+
+console.log(Math.round(Math.PI));
---
+++ Tipos/type-string.js
@@ -0,0 +1,5 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const minusculo = input.toLocaleLowerCase();
+console.log(cidade === minusculo); // false
