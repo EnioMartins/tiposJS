13ec1f0319ae199a1ce3002d0815f775ae2ac243
EnioMartins <eniomartins7985@gmail.com>
2022-10-17 13:14:05 -0300

    Add var e conversoes

---
+++ Tipos/conversao.js
@@ -0,0 +1,10 @@
+// conversao implicita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+
+// conversao explicita
+
+//console.log(numero + Number(numeroString));
---
+++ Tipos/var-let-const.js
@@ -0,0 +1,6 @@
+var altura = 5;
+var comprimento = 7;
+var area;
+area = altura * comprimento;
+console.log(area);
+
